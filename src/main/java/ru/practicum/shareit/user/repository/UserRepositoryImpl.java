d525c2ec304f5163e46d62ebc0d33de437064f32
Eugeniy Belosheykin <111960775+Mastema22@users.noreply.github.com>
2024-02-26 22:39:26 +0300

    Исправил файл repository, добавил map вместо list

--- src/main/java/ru/practicum/shareit/user/repository/UserRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepositoryImpl.java
@@ -2,70 +2,97 @@ package ru.practicum.shareit.user.repository;
 
 import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.exception.CreateDuplicateEmailException;
-import ru.practicum.shareit.exception.UpdateEmailExistsException;
+import ru.practicum.shareit.exception.EntityNotFoundException;
+import ru.practicum.shareit.exception.UserAlreadyExsistExeption;
 import ru.practicum.shareit.user.model.User;
 
+import javax.validation.ValidationException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class UserRepositoryImpl implements UserRepository {
-    private final List<User> userList = new ArrayList<>();
-
-    private int id = 0;
+    private final Map<Long, User> userList = new HashMap<>();
+    private Long id = 0L;
 
     @Override
     public List<User> findAll() {
-        return userList;
+        return (List<User>) userList.values();
     }
 
     @Override
-    public User findById(long userId) {
-        return userList.stream().filter(user -> user.getId() == userId).findFirst().orElseThrow();
+    public User findById(Long userId) {
+        if (!userList.containsKey(userId)) {
+            throw new EntityNotFoundException("Пользователя с ID=" + userId + " нет в текущем списке!");
+        }
+        return userList.values()
+                .stream()
+                .filter(user -> user.getUserId().equals(userId))
+                .findFirst()
+                .orElseThrow();
     }
 
     @Override
     public User save(User user) {
         checkUser(user);
-        user.setId(++id);
-        userList.add(user);
+        user.setUserId(++id);
+        userList.put(user.getUserId(), user);
         return user;
     }
 
     @Override
-    public User put(long id, User user) {
-        checkUser(user);
-        User userResult = new User();
-        for (User user1 : userList) {
-            if (user1.getId() == id) {
-                if (user.getName() != null ) {
-                    if (user1.getName() != user.getName()) {
-                        user1.setName(user.getName());
-                    } else throw new RuntimeException("Имя уже существует в текущей записи!");
-                }
-                if (user.getEmail() != null) {
-                    if (user1.getEmail() != user.getEmail()) {
-                        user1.setEmail(user.getEmail());
-                    } else throw new UpdateEmailExistsException("Email уже существует в текущей записи!");
-                }
-                userResult = user1;
-            }
+    public User put(User user) {
+        if (!userList.containsValue(user)) {
+            throw new EntityNotFoundException("Пользователя с ID=" + user.getUserId() + " нет в текущем списке!");
         }
-        return userResult;
+        if (user.getUserId() == null) {
+            throw new ValidationException("Передан пустой аргумент!");
+        }
+        if (user.getName() == null) {
+            user.setName(userList.get(user.getUserId()).getName());
+        }
+        if (user.getEmail() == null) {
+            user.setName(userList.get(user.getUserId()).getEmail());
+        }
+        if (userList.values()
+                .stream()
+                .filter(user1 -> user1.getEmail().equals(user.getEmail()))
+                .allMatch(user1 -> user1.getUserId().equals(user.getUserId()))) {
+            if (isValidUser(user)) {
+                userList.put(user.getUserId(), user);
+            }
+        } else throw new UserAlreadyExsistExeption("Пользователь с E-mail=" + user.getEmail() + " уже существует!");
+
+
+        return user;
     }
 
     @Override
-    public void delete(long id) {
-        User user = findById(id);
-        userList.remove(user);
+    public void delete(Long userId) {
+        if (!userList.containsKey(userId)) {
+            throw new EntityNotFoundException("Пользователя с ID=" + userId + " нет в текущем списке!");
+        }
+        userList.remove(userId);
     }
 
     public void checkUser(User user) {
-        userList.forEach(user1 -> {
+        userList.forEach((aLong, user1) -> {
             if (user1.getEmail().equals(user.getEmail())) {
-                throw new CreateDuplicateEmailException("Email " + user.getEmail() + " уже существует в текущей записи!");
+                throw new CreateDuplicateEmailException("Пользователь с email=" + user.getEmail() + " уже есть!");
             }
         });
-
     }
+
+    private boolean isValidUser(User user) {
+        if (!user.getEmail().contains("@")) {
+            throw new ValidationException("Некорректный e-mail пользователя: " + user.getEmail());
+        }
+        if ((user.getName().isEmpty()) || (user.getName().contains(" "))) {
+            throw new ValidationException("Некорректный логин пользователя: " + user.getName());
+        }
+        return true;
+    }
+
 }
